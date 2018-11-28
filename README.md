47192158207431ad5201bd4ebd437a4fea322307
fadilhakim <fadil.nylon@gmail.com>
2018-11-28 15:51:00 +0700

    blab

---
+++ README.md
@@ -0,0 +1,7 @@
+# devHouse website beta
+## instalation step :
+
+### 1. git clone
+### 2. npm install
+### 3. nodemon -e .ejs,.js app.js
+### 4. Open port 3002
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ejs') // register the template engine
+
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('comingsoon'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
